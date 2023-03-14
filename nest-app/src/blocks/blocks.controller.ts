6124fcea356c44eaeb0adcc70c4287fef5273d7d
mgibson-casper <matthew@casperlabs.io>
2023-03-14 10:13:46 -0600

    spike-37: add class validators to setup query params validation

--- nest-app/src/blocks/blocks.controller.ts
+++ nest-app/src/blocks/blocks.controller.ts
@@ -1,14 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
-import { BlocksService } from './blocks.service';
+import {
+  Controller,
+  Get,
+  Optional,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Query,
+} from "@nestjs/common";
+import { Transform } from "class-transformer";
+import {
+  IsBoolean,
+  IsDate,
+  IsIn,
+  IsNumber,
+  IsNumberString,
+  IsOptional,
+  IsString,
+} from "class-validator";
+import { BlocksService } from "./blocks.service";
 
-@Controller('blocks')
+export class BlocksQueryDtp {
+  @Transform(({ value }) => parseInt(value, 10))
+  @IsNumber()
+  @IsOptional()
+  public pageNum: number = 1;
+
+  @Transform(({ value }) => parseInt(value, 10))
+  @IsNumber()
+  public count: number = 10;
+
+  // TODO: add @IsIn check?
+  @IsString()
+  @IsOptional()
+  public sortBy: string;
+
+  @IsIn(["desc", "asc"])
+  @IsString()
+  @IsOptional()
+  public orderBy: string = "desc";
+}
+
+@Controller("blocks")
 export class BlocksController {
   constructor(private readonly blocksService: BlocksService) {}
 
-  @Get('latest-block')
+  @Get("latest-block")
   async getLatestBlock() {
     const latestBlock = await this.blocksService.getLatestBlock();
 
     return latestBlock;
+  }
+
+  @Get()
+  async getBlocks(@Query() query: BlocksQueryDtp) {
+    console.log({ query });
   }
 }
