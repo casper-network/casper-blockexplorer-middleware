fc173573dc980ad149e0e8cbb3f22131ca9ffc88
mgibson-casper <matthew@casperlabs.io>
2023-03-15 08:41:53 -0600

    spike-37: add validators classes to module

--- nest-app/src/validators/validators.module.ts
+++ nest-app/src/validators/validators.module.ts
@@ -1,8 +1,10 @@
 import { CacheModule, Module } from "@nestjs/common";
+import { ValidatorsController } from "./validators.controller";
+import { ValidatorsService } from "./validators.service";
 
 @Module({
   imports: [CacheModule.register({ ttl: Number.MAX_VALUE, max: 2 ** 19 })],
-  controllers: [],
-  providers: [],
+  controllers: [ValidatorsController],
+  providers: [ValidatorsService],
 })
 export class ValidatorsModule {}
