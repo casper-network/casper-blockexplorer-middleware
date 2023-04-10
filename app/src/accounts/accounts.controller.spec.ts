9589b86fed82b77aedc4f9bfb800a3c3013e214b
mgibson-casper <matthew@casperlabs.io>
2023-04-10 14:32:48 -0600

    feat-50: add account stub

--- app/src/accounts/accounts.controller.spec.ts
+++ app/src/accounts/accounts.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AccountsController } from "./accounts.controller";
 import { AccountsService } from "./accounts.service";
+import { getAccountStub } from "./stubs/accounts.stub";
 
 jest.mock("./accounts.service");
 
@@ -24,28 +25,17 @@ describe("AccountsController", () => {
     let account: any;
 
     beforeEach(() => {
-      // TODO: put the hashOrPublicKey in stub
-      account = accountsService.getAccount(
-        "01000e6fce753895c0d08d5d6af62db4e9b0d070f10e69e2c6badf977b29bbeeee"
-      );
+      account = accountsService.getAccount(getAccountStub()._accountHash);
     });
 
     it("should be called with hashOrPublicKey", () => {
       expect(accountsService.getAccount).toHaveBeenCalledWith(
-        "01000e6fce753895c0d08d5d6af62db4e9b0d070f10e69e2c6badf977b29bbeeee"
+        getAccountStub()._accountHash
       );
     });
 
     it("should return account", () => {
-      expect(account).toEqual({
-        _accountHash:
-          "account-hash-e386a6e2d67ab4c7af524f0b7f60fa77fe420a189309b613f359ccd83c27807a",
-        namedKeys: [],
-        mainPurse:
-          "uref-7e38074b9fe8435ddd12ad892a3a06ecedc0cd71194fa35d061726e21743865b-007",
-        associatedKeys: [],
-        actionThresholds: { deployment: 1, keyManagement: 1 },
-      });
+      expect(account).toEqual(getAccountStub());
     });
   });
 });
---
+++ app/src/accounts/stubs/accounts.stub.ts
@@ -0,0 +1,11 @@
+import { StoredValue } from "casper-js-sdk";
+
+export const getAccountStub = () => ({
+  _accountHash:
+    "account-hash-e386a6e2d67ab4c7af524f0b7f60fa77fe420a189309b613f359ccd83c27807a",
+  namedKeys: [],
+  mainPurse:
+    "uref-7e38074b9fe8435ddd12ad892a3a06ecedc0cd71194fa35d061726e21743865b-007",
+  associatedKeys: [],
+  actionThresholds: { deployment: 1, keyManagement: 1 },
+});
