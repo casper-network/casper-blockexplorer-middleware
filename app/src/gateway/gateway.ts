1f8c70d4dcb6096e894d6fc990563833d00d4d0e
mgibson-casper <matthew@casperlabs.io>
2023-05-23 13:04:22 -0600

    feat-69: clean up gateway service

--- app/src/gateway/gateway.ts
+++ app/src/gateway/gateway.ts
@@ -23,33 +23,18 @@ export class MyGateway
     console.log("Websocket gateway initialized.");
   }
 
-  handleConnection(client: Socket, ...args: any[]) {
+  handleConnection(client: Socket) {
     const sockets = this.io.sockets;
 
     console.log("WS client with id: ", client.id);
     console.log("Number of connected sockets: ", sockets.size);
-
-    // this.io.to(client.id).emit(
-    //   "test_message",
-    //   JSON.stringify({
-    //     first: "this is the first property",
-    //     second: false,
-    //     third: 1000,
-    //   })
-    // );
   }
 
   handleEvent(event: string, ...args: any[]) {
-    // TODO: figure out how to pass the clientID here like in handleConnection method
-    // Or actually do we really care since we'll want to emit to all clients connected?
-    // I should really figure out a way to test that we can emit to multiple clients at once
-    console.log({ event });
-    console.log({ args });
-
     this.io.emit(event, JSON.stringify(args));
   }
 
   handleDisconnect(client: Socket) {
-    console.log("Websocket gateway disconnect.");
+    console.log(`Websocket gateway disconnected from. ${client.id}`);
   }
 }
